709aead49af4259498865a08a3fc8e93ab55d57f
avid-modal-4107 <norbertl@buffalo.edu>
2018-12-25 03:33:51 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,6 +2,11 @@ def reverse_each_word(input)
   input.split().collect do |word|
     word.reverse
   end
+<<<<<<< HEAD
   .join(" ")
 end
 
+=======
+  input
+end
+>>>>>>> cd2f9ce5286f0d8ff1ef77c31fb5d21d31849b01
